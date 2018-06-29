80b632698c62158d4ff1fdd45fc8c306152a4bee
Leicson Borges Palhano <leicsonbp@outlook.com>
2018-06-29 00:19:17 -0300

    New folders and changes on scss archives

---
+++ client/src/js/app/models/Gastos.js
@@ -0,0 +1,14 @@
+class Gastos{
+
+    constructor(value,date,description,payment,debtor,form){
+        this._value=value;
+        this._date=date;
+        this._description=description;
+        this._payment=payment;
+        this._debtor=debtor;
+        this._form=form;
+    }
+
+    
+
+}
--- js/app/models/Gastos.js
+++
@@ -1,5 +0,0 @@
-class Gastos{
-
-    constructor(value,)
-
-}
